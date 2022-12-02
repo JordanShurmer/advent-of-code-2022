2e6455bdbba765367a0457cc2a6b1ce609e1646c
Jordan Shurmer <jordan_shurmer@discovery.com>
2022-12-01 22:37:34 -0500

    day one. done with Bethany's high level plan

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").unwrap();
+
+    // Separate/Group by blank line
+    let elves_calories = input.split("\n\n");
+
+
+    // sum each group
+    let mut totals: Vec<i32> = elves_calories.map(|elf| {
+        elf.split("\n")
+            .map(str::parse::<i32>)
+            .filter_map(|food| food.ok())
+            .sum::<i32>()
+    }).collect();
+
+    // find the highest
+    totals.sort_by(|a, b| b.cmp(a));
+    println!("Single highest: {}", totals.first().unwrap());
+    totals.truncate(3);
+    println!("Three highest: {}", totals.into_iter().sum::<i32>());
+
 }
+
+
