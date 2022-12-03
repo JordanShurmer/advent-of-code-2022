91450cf459b664bd8c7723f0741f626e8d6ea978
Jordan Shurmer <jordan_shurmer@discovery.com>
2022-12-03 15:23:16 -0500

    day 2 part 2 correct

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -27,12 +27,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("C Z", 3),
     ].into_iter().collect();
 
-    let total = input.lines()
+    let part_2: HashMap<&str, i32> = [
+        // loose
+        ("X", 0),
+        // draw
+        ("Y", 3),
+        // win
+        ("Z", 6),
+
+        // rocks
+        ("B X", 1), //loose
+        ("A Y", 1), //draw
+        ("C Z", 1), //win
+
+        // papers
+        ("C X", 2), //loose
+        ("B Y", 2), //draw
+        ("A Z", 2), //win
+
+        // scissors
+        ("A X", 3), //loose
+        ("C Y", 3), //draw
+        ("B Z", 3), //win
+    ].into_iter().collect();
+
+    let (total_1, total_2) = input.lines()
         .filter_map(|line| line.ok())
-        .fold(0, |acc, round| {
-            return acc + part_1[&round[2..]] + part_1.get(&round.as_str()).unwrap_or(&0);
+        .fold((0, 0), |acc, round| {
+            return (
+                acc.0 + part_1[&round[2..]] + part_1.get(&round.as_str()).unwrap_or(&0),
+                acc.1 + part_2[&round[2..]] + part_2.get(&round.as_str()).unwrap_or(&0)
+            )
         });
 
-    println!("{}", total);
+    println!("part 1: {}", total_1);
+    println!("part 2: {}", total_2);
     Ok(())
 }
