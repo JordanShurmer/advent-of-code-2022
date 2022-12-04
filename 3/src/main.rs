4673fd0f86af2896e33a6c186bd20c84bee852cf
Jordan Shurmer <jordan_shurmer@discovery.com>
2022-12-03 20:39:04 -0500

    day 3 part 1 correct

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = BufReader::new(File::open("input.txt")?);
+
+    let total: u32 = input.lines()
+        .filter_map(|line| line.ok())
+
+        // split the rucksack into equal compartments
+        .map(|rucksack| {
+            let (compartment_1, compartment_2) = rucksack.split_at(rucksack.len()/2);
+            // not sure why I cant just return the .split_at directly.
+            // spent to long trying things.. went with the explicit thing here
+            (compartment_1.to_owned(), compartment_2.to_owned())
+        })
+
+        // get the chars of each compartment, in a Set
+        .map(|(compartment_1, compartment_2)| (
+            compartment_1.chars().collect::<HashSet<_>>(),
+            compartment_2.chars().collect::<HashSet<_>>()
+        ))
+
+        // .inspect(|thing| println!("{:?}", thing))
+
+        // get the intersection of both compartments
+        .map(|(set_1, set_2)| set_1.intersection(&set_2).copied().collect::<HashSet<_>>())
+
+        // .inspect(|thing| println!("{:?}", thing))
+
+        // compute the score of all combined
+        .flatten()
+        .map(score)
+
+        .sum();
+
+    println!("total: {}", total);
+
+    Ok(())
+}
+
+
+fn score(char: char) -> u32 {
+    if char.is_uppercase() {
+        // A-Z is 27-52
+        return (char as u32) - ('A' as u32) + 27;
+    }
+    // a-z is 1-26
+    return (char as u32) - ('a' as u32) + 1;
+}
+
+#[test]
+fn scores() {
+    assert_eq!(score('A'), 27);
+    assert_eq!(score('B'), 28);
+    assert_eq!(score('Z'), 52);
+    assert_eq!(score('a'), 1);
+    assert_eq!(score('b'), 2);
+    assert_eq!(score('z'), 26);
 }
