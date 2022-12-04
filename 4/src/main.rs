0aabd0b142aa44ec75420cd0e9751d31c72163fe
Jordan Shurmer <jordan_shurmer@discovery.com>
2022-12-04 16:24:18 -0500

    day 4 part 2! add .overlaps for Range

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -21,12 +21,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .filter(|(section_1, section_2)| {
             section_1.contains(section_2) || section_2.contains(section_1)
         })
-
         // .inspect(|thing| println!("{:?}", thing))
-        
         .count();
 
-    println!("total: {}", count);
+    println!("fully contained: {}", count);
+
+    let input = BufReader::new(File::open(format!(
+        "{}/input.txt",
+        env!("CARGO_MANIFEST_DIR")
+    ))?);
+
+    let count = input
+        .lines()
+        .filter_map(|line| line.ok())
+
+        // turn the strings into "Range"s
+        .map::<(Range, Range), _>(|pair| {
+            let (sections_1, sections_2) = pair.split_once(",").unwrap();
+            (sections_1.into(), sections_2.into())
+        })
+
+        // filter for only pairs with overlapping ranges
+        .filter(|(section_1, section_2)| section_1.overlaps(section_2) || section_2.overlaps(section_1))
+
+        // .inspect(|thing| println!("{:?}", thing))
+        .count();
+
+    println!("any overlap: {}", count);
     Ok(())
 }
 
@@ -40,11 +61,28 @@ impl Range {
     pub fn contains(&self, other: &Range) -> bool {
         return self.start <= other.start && self.end >= other.end;
     }
+
+    pub fn overlaps(&self, other: &Range) -> bool {
+        return
+            // other.end overlaps
+            (other.end >= self.start && other.end <= self.end) ||
+
+            // other.start overlaps
+            (other.start >= self.start && other.start <= self.end)
+        ;
+    }
 }
+
 #[test]
 fn range_contains() {
     let r: Range = "5-10".into();
     assert!(r.contains(&"6-8".into()));
+}
+#[test]
+fn range_overlaps() {
+    let r: Range = "5-10".into();
+    assert!(r.overlaps(&"1-5".into()));
+    assert!(r.overlaps(&"10-20".into()));
 }
 
 impl From<&str> for Range {
@@ -56,6 +94,7 @@ impl From<&str> for Range {
         }
     }
 }
+
 #[test]
 fn range_from_string() {
     let r: Range = "32-55".into();
