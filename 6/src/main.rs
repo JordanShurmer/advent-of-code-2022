79da4dbdef90c9cf680fe0e2c8db225231303d24
Jordan Shurmer <jordan_shurmer@discovery.com>
2022-12-09 22:57:54 -0500

    day-6 correct

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -3,20 +3,32 @@ use std::{collections::HashSet, fs::read_to_string};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_to_string(format!("{}/input.txt", env!("CARGO_MANIFEST_DIR"))).unwrap();
 
-    let answer: [char; 4];
 
-    let _ = input
-        .chars()
-        .collect::<Vec<_>>()
+    // a list of (index, char)
+    let all_char= input.char_indices().collect::<Vec<_>>();
+
+     let answer  = all_char
+
+         //grouped into groups of 4
         .windows(4)
-        .take_while(|group| {
-            let unique_chars = group.iter().collect::<HashSet<_>>();
-            if unique_chars.len() == 4 {
-                answer = *group.try_into().unwrap();
-                return false;
+
+        .find_map(|group| {
+
+            //turn each group of 4 into a HashSet
+            let unique_chars = group.iter().map(|(_, c)| c).collect::<HashSet<_>>();
+
+            return match unique_chars.len() {
+                // if we have 4 elements then there's 4 unique chars
+                // so return the index of the last char
+                4 => Some(group.last().unwrap().0),
+
+                // otherwise there's not
+                _ => None
+                
             }
-            return true;
-        });
+        }).unwrap();
+
+    println!("{:?}", answer+1);
 
     Ok(())
 }
