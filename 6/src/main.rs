0d2d6a42fe1be8949a4c3aaa86a1e940da6de2f8
Jordan Shurmer <jordan@shurmer.family>
2022-12-09 22:31:05 -0500

    wip day-6

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashSet, fs::read_to_string};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = read_to_string(format!("{}/input.txt", env!("CARGO_MANIFEST_DIR"))).unwrap();
+
+    let answer: [char; 4];
+
+    let _ = input
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(4)
+        .take_while(|group| {
+            let unique_chars = group.iter().collect::<HashSet<_>>();
+            if unique_chars.len() == 4 {
+                answer = *group.try_into().unwrap();
+                return false;
+            }
+            return true;
+        });
+
+    Ok(())
 }
